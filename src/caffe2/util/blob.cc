d64f214ef55f052e9d917b289d9dd49737e399f9
rilesdg3 <44391381+rilesdg3@users.noreply.github.com>
2019-11-15 01:10:16 -0600

    Add files via upload

--- src/caffe2/util/blob.cc
+++ src/caffe2/util/blob.cc
@@ -32,7 +32,7 @@ void BlobUtil::Set(const TensorCPU &value, bool force_cuda) {
 }
 
 template<typename T>
-void BlobUtil::Set(const std::vector<int> &dim, const std::vector<T> &data, bool force_cuda) {
+void BlobUtil::Set(int size, const std::vector<int> &dim, const std::vector<T> &data, bool force_cuda) {
 #ifdef WITH_CUDA
   if (force_cuda || blob_.IsType<TensorCUDA>()) {
     //auto tensor = blob_.GetMutable<TensorCUDA>();
@@ -43,11 +43,113 @@ void BlobUtil::Set(const std::vector<int> &dim, const std::vector<T> &data, bool
 #endif
   auto value = Tensor(dim,DeviceType::CPU);
 
-  	  int count = 0;
+  int count = 0;
+  for (int it = 0; it<(size)*dim[0]; ++it) {
+  	  	  	  //std::cout<<"count "<<count<<std::endl;
+  	  		  value.mutable_data<T>()[count] =data[it];
+  	  		  //std::cout<<"value.mutable_data<T>()[count] "<<value.mutable_data<T>()[count]<<std::endl;
+  	  	  count++;
+  	    }
+  	  /*int count = 0;
   	  for (auto& v : data) {
   		  value.mutable_data<T>()[count] =v;
   		  count++;
-  	  }
+  	  }*/
+
+  auto tensor = caffe2::BlobGetMutableTensor(&blob_,DeviceType::CPU);
+  tensor->ResizeLike(value);
+  tensor->ShareData(value);
+}
+
+template<typename T>
+void BlobUtil::Set(const std::vector<int> &dim, const std::vector<T> &data, int start, bool force_cuda) {
+#ifdef WITH_CUDA
+  if (force_cuda || blob_.IsType<TensorCUDA>()) {
+    //auto tensor = blob_.GetMutable<TensorCUDA>();
+	auto tensor =  caffe2::BlobGetMutableTensor(blob_,DeviceType::CUDA);
+    tensor->CopyFrom(value);
+    return;
+  }
+#endif
+  auto value = Tensor(dim,DeviceType::CPU);
+
+  int count = 0;
+  for (int it = start*dim[0]; it<(start+1)*dim[0]; ++it) {
+  	  	  	  //std::cout<<"count "<<count<<std::endl;
+  	  		  value.mutable_data<T>()[count] =data[it];
+  	  		  //std::cout<<"value.mutable_data<T>()[count] "<<value.mutable_data<T>()[count]<<std::endl;
+  	  	  count++;
+  	    }
+  	  /*int count = 0;
+  	  for (auto& v : data) {
+  		  value.mutable_data<T>()[count] =v;
+  		  count++;
+  	  }*/
+
+  auto tensor = caffe2::BlobGetMutableTensor(&blob_,DeviceType::CPU);
+  tensor->ResizeLike(value);
+  tensor->ShareData(value);
+}
+
+template<typename T>
+void BlobUtil::Set(const std::vector<int> &dim, const std::vector<std::vector<T> > &data, int start, bool force_cuda) {
+#ifdef WITH_CUDA
+  if (force_cuda || blob_.IsType<TensorCUDA>()) {
+    //auto tensor = blob_.GetMutable<TensorCUDA>();
+	auto tensor =  caffe2::BlobGetMutableTensor(blob_,DeviceType::CUDA);
+    tensor->CopyFrom(value);
+    return;
+  }
+#endif
+  auto value = Tensor(dim,DeviceType::CPU);
+
+
+  int count = 0;
+
+  int end = (start+1)*dim[0];
+
+  for (int it = start*dim[0]; it<end; it++) {
+
+	  for(auto& v1 : data[it])
+	  {
+		  //std::cout<<"count "<<count<<std::endl;
+		  value.mutable_data<T>()[count] =v1;
+		  //std::cout<<"value.mutable_data<T>()[count] "<<value.mutable_data<T>()[count]<<std::endl;
+		  count++;
+	  }
+  }
+
+
+/*  	  int count = 0;
+  	  for (auto& v : data) {
+  		  for(auto& v1 : v)
+  		  {
+  			  std::cout<<"count "<<count<<std::endl;
+  			  value.mutable_data<T>()[count] =v1;
+  		  std::cout<<"value.mutable_data<T>()[count] "<<value.mutable_data<T>()[count]<<std::endl;
+  		  }
+  		  count++;
+  	  }*/
+
+  auto tensor = caffe2::BlobGetMutableTensor(&blob_,DeviceType::CPU);
+  tensor->ResizeLike(value);
+  tensor->ShareData(value);
+}
+
+template<typename T>
+void BlobUtil::Set(const std::vector<int> &dim, const T &data, bool force_cuda) {
+#ifdef WITH_CUDA
+  if (force_cuda || blob_.IsType<TensorCUDA>()) {
+    //auto tensor = blob_.GetMutable<TensorCUDA>();
+	auto tensor =  caffe2::BlobGetMutableTensor(blob_,DeviceType::CUDA);
+    tensor->CopyFrom(value);
+    return;
+  }
+#endif
+  auto value = Tensor(dim,DeviceType::CPU);
+  int count = 0;
+
+  		  value.mutable_data<T>()[count] =data;
 
   auto tensor = caffe2::BlobGetMutableTensor(&blob_,DeviceType::CPU);
   tensor->ResizeLike(value);
@@ -61,7 +163,11 @@ void BlobUtil::Print(const std::string &name, int max) {
 
 //or you could just remove the below and place put the function implementation inside the header file and delete it from the cpp file
 //see this https://stackoverflow.com/questions/10632251/undefined-reference-to-template-function for better explaination
-template void BlobUtil::Set<float>(const std::vector<int> &, const std::vector<float> &, bool);
-template void BlobUtil::Set<int>(const std::vector<int> &, const std::vector<int> &, bool);
+template void BlobUtil::Set<float>(int, const std::vector<int> &, const std::vector<float> &, bool);
+template void BlobUtil::Set<float>(const std::vector<int> &, const std::vector<float> &, int, bool);
+template void BlobUtil::Set<float>(const std::vector<int> &, const std::vector<std::vector<float> > &, int , bool);
+template void BlobUtil::Set<int>(const std::vector<int> &, const std::vector<int> &, int, bool);
+template void BlobUtil::Set<int>(const std::vector<int> &, const std::vector<std::vector<int> > &, int ,  bool);
+template void BlobUtil::Set<int>(const std::vector<int> &, const int &, bool);
 
 }  // namespace caffe2
